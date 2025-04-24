e1c42cf75d0bf49a1d0d309b14782ca089b4343e
Rachel Beneroff <rachel@joylabhq.com>
2025-04-23 23:35:49 -0700

    working on vercel release

---
+++ public/js/app.js
@@ -0,0 +1,33 @@
+(function() {
+  angular
+    .module('rbeneroff', ['ui.router'])
+    .config(MainRouter)
+  .run(
+  ['$rootScope', '$state', '$stateParams',
+      function ($rootScope, $state, $stateParams){
+          $rootScope.$state = $state;
+          $rootScope.$stateParams = $stateParams;
+  }]);
+
+  MainRouter.$inject = ['$stateProvider', '$urlRouterProvider', '$locationProvider'];
+
+  function MainRouter($stateProvider, $urlRouterProvider, $locationProvider) {
+
+    $stateProvider
+    .state('home', {
+      url: '/',
+      templateUrl: 'home.html',
+    })
+    .state('hume', {
+      url: '/proposals/hume',
+      templateUrl: 'hume.html',
+    })
+    $urlRouterProvider.otherwise('/');
+
+    $locationProvider.html5Mode({
+    enabled: true,
+    requireBase: false
+    }); //locationProvider
+
+  } //MainRouter
+})(); //IIFE
